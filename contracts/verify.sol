d5aa0ab24a8028eb755c1f71f9e1f3aea4c7609d
nkrishang <62195808+nkrishang@users.noreply.github.com>
2020-08-11 18:03:01 -0400

    trimmed down the contract

--- contracts/verify.sol
+++ contracts/verify.sol
@@ -7,23 +7,23 @@ import "@nomiclabs/buidler/console.sol";
 
 contract Verification {
 
+    
     bytes32 public hash;
     bytes public signature;
 
-    constructor(string memory toHash) public {
-        bytes32 temp = keccak256(abi.encode(toHash));
-        hash = ECDSA.toEthSignedMessageHash(temp);
+    constructor(bytes32 tempHash) public {
+        hash = ECDSA.toEthSignedMessageHash(tempHash);
     }
 
     function setSignature(bytes calldata _signature) external {
         signature = _signature;
     }
-
-    function verify(address signer) external view returns(bool) {
+    
+    function recoverAddr(address vendor) external view returns(bool) {
         
         address recoveredAddr = ECDSA.recover(hash, signature);
         //console.log(recoveredAddr);
-        return recoveredAddr == signer;
+        return recoveredAddr == vendor;
     }
 
 }
