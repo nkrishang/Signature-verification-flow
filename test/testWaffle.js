e30f6be1a37d6f57971133213db4a11f8b3d21b8
nkrishang <62195808+nkrishang@users.noreply.github.com>
2020-08-11 18:17:07 -0400

    Added fixSignature function
    The signature doesn't change even when put through fixSignature.

--- test/testWaffle.js
+++ test/testWaffle.js
@@ -18,7 +18,7 @@ describe("Verification", function() {
 
     it("Should verify that the signer signed the hash the contract was deployed with.", async function() {
 
-        const _signature = await signer.signMessage(hashMessage);
+        const _signature = fixSignature(await signer.signMessage(hashMessage));
         
 
         await verify.setSignature(_signature);
@@ -32,3 +32,15 @@ describe("Verification", function() {
         expect(await verify.recoverAddr(signerAddr)).to.equal(true);
     })
 })
+
+function fixSignature (signature) {
+    // in geth its always 27/28, in ganache its 0/1. Change to 27/28 to prevent
+    // signature malleability if version is 0/1
+    // see https://github.com/ethereum/go-ethereum/blob/v1.8.23/internal/ethapi/api.go#L465
+    let v = parseInt(signature.slice(130, 132), 16);
+    if (v < 27) {
+      v += 27;
+    }
+    const vHex = v.toString(16);
+    return signature.slice(0, 130) + vHex;
+}
